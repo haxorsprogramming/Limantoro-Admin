8f155627f3bf7370f8fc1479d74d1faec34c1f64
Haxorsprogramming <haxors@uinsu.ac.id>
2021-11-16 14:01:44 +0700

    'update'

--- routes/web.php
+++ routes/web.php
@@ -18,16 +18,16 @@ Route::get('/app/beranda', [C_Page::class, 'berandaPage']);
 // supplier 
 Route::get('/app/supplier', [C_Supplier::class, 'supplierPage']);
 Route::get('/app/supplier/datatable', [C_Supplier::class, 'jsonDatatable']);
-Route::post('/app/supplier/tambah/proses', [C_Supplier::class, 'prosesTambahSupplier']);
 Route::get('/app/supplier/{codeSupplier}/edit/data', [C_Supplier::class, 'editDataSupplier']);
+Route::post('/app/supplier/tambah/proses', [C_Supplier::class, 'prosesTambahSupplier']);
 Route::post('/app/supplier/edit/proses', [C_Supplier::class, 'prosesUpdateSupplier']);
 Route::post('/app/supplier/delete/proses', [C_Supplier::class, 'prosesDeleteSupplier']);
 // customer 
 Route::get('/app/customer', [C_Customer::class, 'customerPage']);
 // material 
 Route::get('/app/material', [C_Material::class, 'materialPage']);
-Route::post('/app/material/tambah/proses', [C_Material::class, 'prosesTambahMaterial']);
 Route::get('/app/material/{codeMaterial}/edit/data', [C_Material::class, 'editDataMaterial']);
+Route::post('/app/material/tambah/proses', [C_Material::class, 'prosesTambahMaterial']);
 Route::post('/app/material/edit/proses', [C_Material::class, 'prosesUpdateMaterial']);
 // karyawan 
 Route::get('/app/karyawan', [C_Karyawan::class, 'karyawanPage']);
